7c43055a13c0f81eafbd92062ec217cd3b29c1f9
Arief Syahroni <riefsyah31@gmail.com>
2021-07-15 21:26:24 +0700

    Add new homePage, theme and library gogleFonts

---
+++ lib/theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color backgroundColor = Color(0XFFAF4A53);
+Color backgroundColor1 = Color(0XFFE5E5E5);
+Color backgroundColor2 = Color(0XFFECC2B9);
+Color whiteColor = Color(0XFFFFFFFF);
+Color blackColor = Color(0XFF1E2237);
+
+TextStyle titleTextStyle = GoogleFonts.lato(
+  fontWeight: FontWeight.w700,
+);
+
+TextStyle subTitleTextStyle = GoogleFonts.lato(
+  fontWeight: FontWeight.w500,
+);
