ae39eae7b2bdc7a8c9b71422367ad3318c3dd4b6
Arief Syahroni <riefsyah31@gmail.com>
2021-07-16 20:50:42 +0700

    add header background

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,47 @@
+import 'package:find_freelancer/theme.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 320,
+              color: backgroundColor,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Opacity(
+                      opacity: 0.1,
+                      child: Image.asset(
+                        'assets/illustration_cover.png',
+                        height: 120,
+                        width: 120,
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Image.asset(
+                      'assets/person_cover.png',
+                      width: 170,
+                      height: 240,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/theme.dart
+++ lib/theme.dart
@@ -6,6 +6,7 @@ Color backgroundColor1 = Color(0XFFE5E5E5);
 Color backgroundColor2 = Color(0XFFECC2B9);
 Color whiteColor = Color(0XFFFFFFFF);
 Color blackColor = Color(0XFF1E2237);
+Color redDarkLight = Color(0XFFC16F77);
 
 TextStyle titleTextStyle = GoogleFonts.lato(
   fontWeight: FontWeight.w700,
