4f72593be9b061d172b74212f0e161673f5567a9
Arief Syahroni <riefsyah31@gmail.com>
2021-07-17 11:45:05 +0700

    add some assets and new widgetPage for topFreelancer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:find_freelancer/theme.dart';
 import 'package:find_freelancer/widget/most_populer_card.dart';
+import 'package:find_freelancer/widget/top_freelancer_card.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -201,161 +202,22 @@ class HomePage extends StatelessWidget {
                       SizedBox(
                         height: 24,
                       ),
-                      Padding(
-                        padding: EdgeInsets.only(right: defaultMargin),
-                        child: Row(
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.only(bottom: 33),
-                              child: Container(
-                                width: 80,
-                                height: 80,
-                                decoration: BoxDecoration(
-                                  color: backgroundColor2,
-                                  borderRadius: BorderRadius.circular(15),
-                                  image: DecorationImage(
-                                    image: AssetImage(
-                                      'assets/person1.png',
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              width: 16,
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(
-                                bottom: 22,
-                              ),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    'Andrea Hutama Karya',
-                                    style: titleTextStyle.copyWith(
-                                        fontSize: 17, color: blackColor),
-                                  ),
-                                  SizedBox(
-                                    height: 4,
-                                  ),
-                                  Text(
-                                    'Programmer',
-                                    style: subTitleTextStyle.copyWith(
-                                      fontSize: 14,
-                                      color: blackColor.withOpacity(0.5),
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 9,
-                                  ),
-                                  Row(
-                                    children: [
-                                      Icon(
-                                        Icons.star,
-                                        size: 15,
-                                        color: Colors.amber,
-                                      ),
-                                      Text(
-                                        '4.9',
-                                        style: subTitleTextStyle.copyWith(
-                                            fontSize: 13),
-                                      ),
-                                      SizedBox(
-                                        width: 10,
-                                      ),
-                                      Icon(
-                                        Icons.location_on,
-                                        size: 12,
-                                        color: blackColor.withOpacity(0.4),
-                                      ),
-                                      SizedBox(
-                                        width: 3,
-                                      ),
-                                      Text(
-                                        'Bandung',
-                                        style: subTitleTextStyle.copyWith(
-                                          fontSize: 13,
-                                          color: blackColor.withOpacity(0.4),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                  SizedBox(
-                                    height: 15,
-                                  ),
-                                  Row(
-                                    children: [
-                                      Container(
-                                        height: 22,
-                                        width: 80,
-                                        decoration: BoxDecoration(
-                                          color: backgroundColor,
-                                          borderRadius:
-                                              BorderRadius.circular(15),
-                                        ),
-                                        child: Center(
-                                          child: Text(
-                                            'HIRE ME',
-                                            style: subTitleTextStyle.copyWith(
-                                              fontSize: 12,
-                                              color: whiteColor,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                      SizedBox(
-                                        width: 8,
-                                      ),
-                                      Container(
-                                        height: 22,
-                                        width: 80,
-                                        decoration: BoxDecoration(
-                                          color: backgroundColor1,
-                                          borderRadius:
-                                              BorderRadius.circular(15),
-                                          border: Border.all(
-                                            color: backgroundColor,
-                                          ),
-                                        ),
-                                        child: Center(
-                                          child: Text(
-                                            'PROFILE',
-                                            style: subTitleTextStyle.copyWith(
-                                              fontSize: 12,
-                                              color: backgroundColor,
-                                            ),
-                                          ),
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Spacer(),
-                            Padding(
-                              padding: EdgeInsets.only(
-                                bottom: 13,
-                              ),
-                              child: Text.rich(
-                                TextSpan(
-                                  text: '\$55',
-                                  style: titleTextStyle.copyWith(fontSize: 14),
-                                  children: [
-                                    TextSpan(
-                                      text: '/hr',
-                                      style: subTitleTextStyle.copyWith(
-                                        fontSize: 12,
-                                        color: Colors.black.withOpacity(0.4),
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
+                      TopFreelancerCard(
+                        imageUrl: 'assets/person1.png',
+                        name: 'Andrea Hutama Karya',
+                        jobTitle: 'Programmer',
+                        rating: '4.9',
+                        location: 'Bandung',
+                        price: '55',
+                      ),
+                      Divider(),
+                      TopFreelancerCard(
+                        imageUrl: 'assets/person2.png',
+                        name: 'Stephan Lee',
+                        jobTitle: 'UI/UX Designer',
+                        rating: '4.5',
+                        location: 'Jakarta',
+                        price: '51',
                       ),
                       Divider(),
                       SizedBox(
